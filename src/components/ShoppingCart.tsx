ad06254fd5d376ac7d2ef66ae9adb604d14abca9
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-11-24 15:46:41 +0100

    Added total

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -5,21 +5,26 @@ import ShoppingCartContext from '../context/ShoppingCartContext';
 import axios from 'axios';
 import ShoppingCartItem from "./ShoppingCartItem";
 import ShoppingCartItemType from "../types/ShoppingCartItemType";
+import { formatCurrency } from '../utils/formatCurrency';
 
 function ShoppingCart() {
   const modalElement = document.getElementById("modal")!;
   const { state, setIsCartOpen } = useContext(ShoppingCartContext);
   const [productsState, setProductsState] = useState<ShoppingCartItemType[]>([]);
+  const [total, setTotal] = useState<number>(0);
 
   useEffect(() => {
     async function fetchProducts() {
       const fetchedProducts: ShoppingCartItemType[] = [];
+      let currTotal = 0;
       try {
         for (const cartItem of state) {
           const response = await axios.get(`https://dummyjson.com/products/${cartItem.id}`);
           fetchedProducts.push({ ...response.data, quantity: cartItem.quantity });
+          currTotal += cartItem.quantity * response.data.price;
         }
         setProductsState(fetchedProducts);
+        setTotal(currTotal)
       } catch (err) {
         console.log(err);
       }
@@ -38,6 +43,9 @@ function ShoppingCart() {
     <main>
       <div id="listItems">{productsState.map(item => <ShoppingCartItem {...item} key={item.id} />)}</div>
     </main>
+    <section id="total">
+      <h3>Total: {formatCurrency(total)}</h3>
+    </section>
   </StyledShoppingCart>, modalElement);
 }
 
