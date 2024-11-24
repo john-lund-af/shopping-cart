a658ac94f4aeff41045f46d857cc3ec98a6a3490
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-11-24 11:27:01 +0100

    Displaying Itemscount

--- src/components/ShoppingCartButton.tsx
+++ src/components/ShoppingCartButton.tsx
@@ -1,11 +1,17 @@
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { StyledShoppingCartButton } from "./styles/ShoppingCartButton.styled";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useContext } from "react";
+import ShoppingCartContext from "../context/ShoppingCartContext";
 
 function ShoppingCartButton() {
+  const { state } = useContext(ShoppingCartContext);
+
+  const totalCount = state.reduce((acc, currItem) => acc + currItem.quantity, 0);
+
   return <StyledShoppingCartButton>
     <FontAwesomeIcon icon={faCartShopping} />
-    <div id="itemsCount">3</div>
+    <div id="itemsCount">{totalCount}</div>
   </StyledShoppingCartButton>;
 }
 
